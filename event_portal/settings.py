700df09fd34c6232aa85c1c3708eed15d2746259
mahmoud <msabbahamr@gmail.com>
2026-05-07 20:56:46 +0300

    Use Railway PostgreSQL database

--- event_portal/settings.py
+++ event_portal/settings.py
@@ -48,15 +48,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'event_portal.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Event_Portal',
-        'USER': 'postgres',
-        'PASSWORD': 'hr',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
