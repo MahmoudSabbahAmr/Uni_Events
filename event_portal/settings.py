437c0d1b6e0e4b2803fadc5463785f6cc908b282
mahmoud <msabbahamr@gmail.com>
2026-05-09 16:47:59 +0300

    Fix database

--- event_portal/settings.py
+++ event_portal/settings.py
@@ -49,12 +49,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'event_portal.wsgi.application'
 
+# settings.py
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
